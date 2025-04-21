b90c17bcd5563acc1002ca2350329f18714f75f7
SabbirHasan <sabbirhasananik@gmail.com>
2025-04-22 02:04:28 +0600

    Implement middleware class to catch exception

---
+++ UserManager/UserManager.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using System.Text.Json;
+
+namespace UserManager.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _env = env;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ErrorResponse
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = ex.Message,
+                        StackTrace = ex.StackTrace
+                    }
+                    : new ErrorResponse
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "Internal server error"
+                    };
+                
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+    }
+}
