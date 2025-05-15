5cd73c03fbc4dac282e135728d9dafc07a052fb4
SabbirHasan <sabbirhasananik@gmail.com>
2025-05-15 15:18:51 +0600

    http client demo

---
+++ HttpClientDemo/Program.cs
@@ -0,0 +1,26 @@
+using HttpClientDemo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+// Register HttpClient
+builder.Services.AddHttpClient<HttpService>();
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
